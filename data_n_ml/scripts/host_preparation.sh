372f530bde67926c399e023936fed4e25a42e544
Apinya <apinyarr@hotmail.com>
2023-10-14 22:46:51 -0400

    add command to install GitHub Actions Runner

--- data_n_ml/scripts/host_preparation.sh
+++ data_n_ml/scripts/host_preparation.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#-------- Docker Installation ----------#
 bash <(curl -s https://raw.githubusercontent.com/docker/docker-install/master/install.sh)
 sudo usermod -aG docker ubuntu
 newgrp docker
+#-------- GitHub Runner Installation --------#
+mkdir actions-runner && cd actions-runner
+curl -o actions-runner-linux-x64-2.309.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.309.0/actions-runner-linux-x64-2.309.0.tar.gz
+echo "2974243bab2a282349ac833475d241d5273605d3628f0685bd07fb5530f9bb1a  actions-runner-linux-x64-2.309.0.tar.gz" | shasum -a 256 -c
+mv actions-runner-linux-x64-2.309.0.tar.gz /home/ubuntu/ && chown ubuntu:ubuntu /home/ubuntu/actions-runner-linux-x64-2.309.0.tar.gz
+tar xzf /home/ubuntu/actions-runner-linux-x64-2.309.0.tar.gz -C /home/ubuntu && chown -R ubuntu:ubuntu /home/ubuntu/
+sudo -H -u ubuntu bash -c "/home/ubuntu/config.sh --url https://github.com/apinyarr/devops-doct-cicd --token AESNUZRYUKJLPEPGHSDU3YDFFH6MY --runnergroup Default --name linux --labels linux --work _work"
+sudo  ./svc.sh install && sudo ./svc.sh start
