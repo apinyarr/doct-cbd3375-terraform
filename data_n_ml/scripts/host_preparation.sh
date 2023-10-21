acd6d5206b64fe1c515d54da79dbee5a6ead40c6
Apinya <apinyarr@hotmail.com>
2023-10-21 09:03:55 -0400

    chage runner's repo and key

--- data_n_ml/scripts/host_preparation.sh
+++ data_n_ml/scripts/host_preparation.sh
@@ -9,5 +9,5 @@ curl -o actions-runner-linux-x64-2.309.0.tar.gz -L https://github.com/actions/ru
 echo "2974243bab2a282349ac833475d241d5273605d3628f0685bd07fb5530f9bb1a  actions-runner-linux-x64-2.309.0.tar.gz" | shasum -a 256 -c
 mv actions-runner-linux-x64-2.309.0.tar.gz /home/ubuntu/ && chown ubuntu:ubuntu /home/ubuntu/actions-runner-linux-x64-2.309.0.tar.gz
 tar xzf /home/ubuntu/actions-runner-linux-x64-2.309.0.tar.gz -C /home/ubuntu && chown -R ubuntu:ubuntu /home/ubuntu/
-sudo -H -u ubuntu bash -c "/home/ubuntu/config.sh --url https://github.com/apinyarr/devops-doct-cicd --token AESNUZRYUKJLPEPGHSDU3YDFFH6MY --runnergroup Default --name linux --labels linux --work _work"
+sudo -H -u ubuntu bash -c "/home/ubuntu/config.sh --url https://github.com/apinyarr/doct-cbd3375-dataprocessing --token AESNUZQ56NWHEIJQ4QZFILLFGPMM2 --runnergroup Default --name linux --labels linux --work _work"
 sudo  ./svc.sh install && sudo ./svc.sh start
